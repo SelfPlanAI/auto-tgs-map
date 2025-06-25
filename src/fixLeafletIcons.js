d1ccaadde597823d06b0c24012dd4ee199483b49
SelfPlanAI <jacksonkiing97@gmail.com>
2025-06-25 18:18:11 +1000

    Update fixLeafletIcons.js

--- src/fixLeafletIcons.js
+++ src/fixLeafletIcons.js
@@ -1,11 +1,12 @@
 import L from "leaflet";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import markerRetina from "leaflet/dist/images/marker-icon-2x.png";
 
-// Override default marker icon path
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerRetina,
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
 });
