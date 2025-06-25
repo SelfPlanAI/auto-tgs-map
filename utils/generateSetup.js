91a2464086c4a21a4c4c5e186db6d055775d75b5
SelfPlanAI <jacksonkiing97@gmail.com>
2025-06-25 14:57:17 +1000

    Create generateSetup.js

---
+++ utils/generateSetup.js
@@ -0,0 +1,20 @@
+import {
+  getSignSpacing,
+  getTaperLength,
+  getBufferZone,
+  getConesSpacing
+} from './rules/vicroadsRules';
+
+export function generateTrafficSetup(speed) {
+  const [signA, signB, signC] = getSignSpacing(speed);
+  const taper = getTaperLength(speed);
+  const buffer = getBufferZone(speed);
+  const coneSpacing = getConesSpacing(speed);
+
+  return {
+    signs: { signA, signB, signC },
+    taper,
+    buffer,
+    coneSpacing
+  };
+}
